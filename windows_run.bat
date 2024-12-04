f068e145a7b1187fed5cca6c6fd93ac7f9b20cd5
Andrew Shakinovsky <andrew@binarydog.com>
2024-12-03 19:18:27 -0500

    spelling

--- windows_run.bat
+++ windows_run.bat
@@ -1,6 +1,6 @@
 @echo off
 
-where pythone.exe > NUL 2>&1
+where python.exe > NUL 2>&1
 if %errorlevel% neq 0 (
   echo Python not found! Please install Python and ensure it's in your PATH.
   echo See https://www.python.org/downloads/windows/
